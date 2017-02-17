a72d9aa6d885db6ce50f1f4db3c642f502142be9
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2017-02-16 23:04:55 -0300

    Create a base project with babel and webpack

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Message from './Message.js';
+
+const App = () => (
+  <div>
+    <h1>Meu Primeiro App React</h1>
+    <Message name="World" />
+    <Message name="React" />
+  </div>
+);
+
+export default App;
